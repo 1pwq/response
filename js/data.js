81d46aa21ba21800028fcf6ab82442c2d7536fb3
1pwq <18326995226@qq.com>
2018-01-06 08:58:06 +0800

    response

---
+++ js/data.js
@@ -0,0 +1,12 @@
+Mock.mock('/lists',{
+	'data':[
+		{
+			"swiper1":[{
+				'title':'@title',
+				'text':'@sentence(4,8)',
+				'text2':'@sentence(4,8)',
+				'more':'Learn more',
+			}]
+		}
+	]
+})
